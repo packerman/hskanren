fffe890d18614cfd88193da91d90d61f86398797
Piotr Ackermann <pioacker@gmail.com>
2019-09-15 23:54:29 +0200

    conj2

--- src/MicroKanren.hs
+++ src/MicroKanren.hs
@@ -75,6 +75,9 @@ nevero = nevero
 alwayso :: Goal a v
 alwayso = disj2 success alwayso
 
+conj2 :: Goal a v -> Goal a v -> Goal a v
+conj2 g1 g2 = concatMap g2 . g1
+
 -- |
 -- >>> let [x, y] = Variable <$> ['x', 'y']
 -- >>> let [a, e] = Value <$> ['a', 'e']
