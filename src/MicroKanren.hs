46cf947cb3e06dce4940a0feca9464d8cc9639bc
Piotr Ackermann <pioacker@gmail.com>
2019-10-16 21:40:01 +0200

    Refactor

--- src/MicroKanren.hs
+++ src/MicroKanren.hs
@@ -12,7 +12,7 @@ import MicroKanren.Functions
 import MicroKanren.Internal
 import MicroKanren.Testing
 
-type LogicM a = RWS () (Conj a) Counter
+type LogicM a = RWS () [Goal a] Counter
 
 type Relation a = Expr a -> LogicM a (Goal a)
 
@@ -22,74 +22,55 @@ disj = foldr disj2 failure
 conj :: [Goal a] -> Goal a
 conj = foldr conj2 success
 
-newtype Disj a = Disj { getDisj :: Goal a }
-
-instance Semigroup (Disj a) where
-    (Disj g1) <> (Disj g2) = Disj $ g1 `disj2` g2
-
-instance Monoid (Disj a) where
-    mempty = Disj failure
-
-newtype Conj a = Conj { getConj :: Goal a }
-
-instance Semigroup (Conj a) where
-    (Conj g1) <> (Conj g2) = Conj $ g1 `conj2` g2
-
-instance Monoid (Conj a) where
-    mempty = Conj success
-
 -- |
 -- >>> let [pea, pod] = Value <$> ["pea", "pod"]
--- >>> run (goal failure >> newVar)
+-- >>> run (goal failure >> fresh)
 -- []
--- >>> run (goal (pea === pod) >> newVar)
+-- >>> run (goal (pea === pod) >> fresh)
 -- []
--- >>> run $ do { q <- newVar; goal $ q === pea; pure q }
+-- >>> run $ do { q <- fresh; goal $ q === pea; pure q }
 -- [Value "pea"]
--- >>> run $ do { q <- newVar; goal $ pea === q; pure q }
+-- >>> run $ do { q <- fresh; goal $ pea === q; pure q }
 -- [Value "pea"]
--- >>> run (goal success >> newVar)
+-- >>> run (goal success >> fresh)
 -- [Reified 0]
--- >>> run $ do { q <- newVar; goal $ q === q; pure q }
+-- >>> run $ do { q <- fresh; goal $ q === q; pure q }
 -- [Reified 0]
 run :: LogicM a (Expr a) -> [Expr a]
-run m = let (e, Conj g) = eval m
-        in map (reify e) $ g emptySubst
+run m = let (e, goals) = eval m
+        in map (reify e) $ (conj goals) emptySubst
 
 -- |
 -- >>> let [pea, pod] = Value <$> ["pea", "pod"]
 -- >>> let [olive, oil] = Value <$> ["olive", "oil"]
 -- >>> runWith $ \q -> goal $ q === pea
 -- [Value "pea"]
--- >>> runWith $ \q -> do { x <- newVar; goal $ pea === q }
+-- >>> runWith $ \q -> do { x <- fresh; goal $ pea === q }
 -- [Value "pea"]
--- >>> runWith $ \q -> do { x <- newVar; goal $ pea === x }
+-- >>> runWith $ \q -> do { x <- fresh; goal $ pea === x }
 -- [Reified 0]
--- >>> runWith $ \q -> do { x <- newVar; goal $ list [x] === q }
+-- >>> runWith $ \q -> do { x <- fresh; goal $ list [x] === q }
 -- [Cons (Reified 0) Nil]
--- >>> runWith $ \q -> do { x <- newVar; goal $ x === q }
+-- >>> runWith $ \q -> do { x <- fresh; goal $ x === q }
 -- [Reified 0]
 -- >>> runWith $ \q -> goal $ list [pea, pod] === list[pea, q]
 -- [Value "pod"]
 -- >>> runWith $ \q -> goal $ list [pea, pod] === list[q, pod]
 -- [Value "pea"]
--- >>> runWith $ \q -> do { x <- newVar; goal $ list [q, x] === list [x, pod] }
+-- >>> runWith $ \q -> do { x <- fresh; goal $ list [q, x] === list [x, pod] }
 -- [Value "pod"]
--- >>> runWith $ \q -> do { x <- newVar; goal $ list [x, x] === q }
+-- >>> runWith $ \q -> do { x <- fresh; goal $ list [x, x] === q }
 -- [Cons (Reified 0) (Cons (Reified 0) Nil)]
--- >>> runWith $ \q -> do { x <- newVar; y <- newVar; goal $ list [q, y] === list [list [x, y], x] }
+-- >>> runWith $ \q -> do { x <- fresh; y <- fresh; goal $ list [q, y] === list [list [x, y], x] }
 -- [Cons (Reified 0) (Cons (Reified 0) Nil)]
--- >>> runWith $ \q -> do { x <- newVar; y <- newVar; goal $ list [x, y] === q }
+-- >>> runWith $ \q -> do { x <- fresh; y <- fresh; goal $ list [x, y] === q }
 -- [Cons (Reified 0) (Cons (Reified 1) Nil)]
--- >>> runWith $ \q -> do { x <- newVar; y <- newVar; goal $ list [x, y, x] === q }
+-- >>> runWith $ \q -> do { x <- fresh; y <- fresh; goal $ list [x, y, x] === q }
 -- [Cons (Reified 0) (Cons (Reified 1) (Cons (Reified 0) Nil))]
 -- >>> runWith $ \q -> goal $ disj2 (olive === q) (oil === q)
 -- [Value "olive",Value "oil"]
 runWith :: (Expr a -> LogicM a ()) -> [Expr a]
 runWith f = run $ satisfying f
-
-fresh :: (Expr a -> b) -> LogicM a b
-fresh f = f <$> newVar
 
 -- |
 -- >>> :{
@@ -103,25 +84,25 @@ conde :: [[Goal a]] -> Goal a
 conde = disj . map conj
 
 satisfying :: (Expr a -> LogicM a ()) -> LogicM a (Expr a)
-satisfying f = newVar >>= (\q -> f q >> pure q)
+satisfying f = fresh >>= (\q -> f q >> pure q)
 
-eval :: LogicM a b -> (b, Conj a)
+eval :: LogicM a b -> (b, [Goal a])
 eval m = evalRWS m () defaultCounter
 
 -- |
 -- >>> :{
---      let go = do x1 <- newVar
---                  x2 <- newVar
---                  y <- newVar
+--      let go = do x1 <- fresh
+--                  x2 <- fresh
+--                  y <- fresh
 --                  return (x1 == x2, x1 == y, y == x2)
 --      in fst $ eval go
 -- :}
 -- (False,False,False)
-newVar :: LogicM a (Expr a)
-newVar = Variable <$> state getAndInc
+fresh :: LogicM a (Expr a)
+fresh = Variable <$> state getAndInc
 
 goal :: Goal a -> LogicM a ()
 goal g = goals [g]
 
 goals :: [Goal a] -> LogicM a ()
-goals = tell . mconcat . map Conj
+goals = tell
--- src/Pair.hs
+++ src/Pair.hs
@@ -5,33 +5,19 @@ import MicroKanren.Functions
 import MicroKanren.Testing
 
 -- |
--- >>> :{
---        run $ do
---                  x <- newVar
---                  y <- newVar
---                  goal $ conde [
---                          [teacupo x, teacupo x],
---                          [Value Fail === x, teacupo y]]
---                  pure $ list [x, y]
--- :}
--- [Cons (Value Tea) (Cons (Reified 0) Nil),Cons (Value Fail) (Cons (Value Tea) Nil),Cons (Value Cup) (Cons (Reified 0) Nil),Cons (Value Fail) (Cons (Value Cup) Nil)]
-teacupo :: Expr Symbol -> Goal Symbol
-teacupo t = disj2 (Value Tea === t) (Value Cup === t)
-
--- |
 -- >>> runWith $ \q -> goal =<< caro (values "acorn") q
 -- [Value 'a']
 -- >>> runWith $ \q -> goal =<< caro (values "acorn") (Value 'a')
 -- [Reified 0]
 -- >>> :{
 --      runWith $ \r -> do
---                          x <- newVar
---                          y <- newVar
+--                          x <- fresh
+--                          y <- fresh
 --                          goal =<< caro (list [r, y]) x
 --                          goal $ (Value Pear) === x
 -- :}
 -- [Value Pear]
 caro :: Eq a => Expr a -> Relation a
 caro p a = do
-            d <- newVar
+            d <- fresh
             pure $ (Cons a d) === p
